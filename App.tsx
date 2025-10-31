9a81df5f87561b7b9e7bac1ea194f69ecf596f26
Praneeth Sai Kolla <praneeth.22bce7165@vitapstudent.ac.in>
2025-10-31 22:49:36 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,33 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Practice from "./pages/Practice";
+import MockTest from "./pages/MockTest";
+import Result from "./pages/Result";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/practice/:category" element={<Practice />} />
+          <Route path="/mock-test" element={<MockTest />} />
+          <Route path="/result" element={<Result />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
